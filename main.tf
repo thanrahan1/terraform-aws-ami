c13f6bff2dc5ab4d69e6a1e5a494f3069cab1c3e
thanrahan1 <20422682+thanrahan1@users.noreply.github.com>
2020-03-16 13:26:04 -0400

    Changes
    Changes

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region     = var.region
+  }
+
+resource "aws_instance" "web" {
+  ami           = "ami-07ebfd5b3428b6f4d"
+  instance_type = "t2.micro"
+  count         = 2
+
+  tags = {
+    Name = "${var.instance_name} ${count.index}"
+  }
+}
+
+variable "instance_name" {
+  
+}
